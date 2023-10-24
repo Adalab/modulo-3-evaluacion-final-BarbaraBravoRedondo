09129d0b7ab2c0393a93536eb0d37e6c623e6126
BarbaraBravoRedondo <barbarabravoredondo@gmail.com>
2023-10-24 12:44:56 +0200

    animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,7 @@
 import '../styles/layouts/Header.scss';
-import wow from '../images/wow-title.png';
+
 function Header() {
-  return (
-    <header className="header">
-      <h1>
-        <img className="imgTitle" src={wow} alt="" />
-      </h1>
-    </header>
-  );
+  return <header className="header"></header>;
 }
 
 export default Header;
